53f12e13c911a00d97ee53411d9e655113982e07
松岡 正 <matsuoka@team-lab.com>
2018-10-27 02:02:07 +0900

    googleの設定実装完了

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var port = process.env.PORT || 5000,
   server = http.createServer(app),
   io = require('socket.io').listen(server),
   fs = require('fs'),
-  twitterMod = require('./twitter');
+  twitterMod = require('./twitter'),
+  googleMod = require('./google');
 
 app.use(express.static(__dirname + "/html"))
 server.listen(port);
@@ -24,12 +25,15 @@ io.sockets.on('connection', function(socket) {
 
   socket.on('sign_up', function(data) {
     if (data.twitterUsername != "" && data.twitterPassword != "") {
-      twitterMod.login(data.twitterUsername, data.twitterPassword);
+      // twitterMod.login(data.twitterUsername, data.twitterPassword);
+      console.log("received username : " + data.googleMailAddress + " pass : " + data.googlePassword);
+      googleMod.login(data.googleMailAddress,data.googlePassword);
     }
   })
 
   socket.on('safetyfirst', function(data) {
     console.log("safetyfirst");
-    twitterMod.privacy_setting(0);
+    // twitterMod.privacy_setting(0);
+    googleMod.privacy_setting(0);
   })
 });
--- google.js
+++ google.js
@@ -6,40 +6,189 @@ const webdriver = require('selenium-webdriver'),
 const expect = require('expect.js');
 const assert = require('assert');
 const async = require('async');
+let driver;
+var timer;
 
-async function main(username,password) {
-  let driver = await new webdriver.Builder()
+const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
+
+async function main(username, password) {
+  driver = await new webdriver.Builder()
     .withCapabilities(webdriver.Capabilities.chrome())
     .build();
 
   await driver.get('https://www.google.co.jp/')
-  const name = await driver.findElement(By.xpath('//input[@name="session[username_or_email]"]'));
-  const pass = await driver.findElement(By.xpath('//input[@name="session[password]"]'));
-  // await name.sendKeys('test41003724');
-  // await pass.sendKeys('123qwe');
-  await name.sendKeys(username);
-  await pass.sendKeys(password);
-  await pass.sendKeys(Key.ENTER);
 
-  await driver.get('http://twitter.com/settings/safety');
+  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//a[@id="gb_70"]'), 1000)));
+  await driver.findElement(By.xpath('//a[@id="gb_70"]')).click();
+
+  console.log("username : " + username + " pass : " + password);
+
+  //input username
+  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//input[@id="identifierId"]'), 1000)));
+  await driver.findElement(By.xpath('//input[@id="identifierId"]')).sendKeys(username);
+  await driver.findElement(By.xpath('//div[@id="identifierNext"]')).click();
+
+  //input password
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//input[@name="password" and @type="password"]'), 1000)));
+  await driver.findElement(By.xpath('//input[@name="password" and @type="password"]')).sendKeys(password);
+  await driver.findElement(By.xpath('//div[@id="passwordNext"]')).click();
 
 
-  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//input[@id="user_protected"]'), 1000)));
-  await new Promise(resolve => setTimeout(resolve, 2000));
-  await driver.findElement(By.xpath('//input[@id="user_protected"]')).click();
+}
+
+async function setting(index){
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await driver.get('https://myaccount.google.com/activitycontrols');
+  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//body[@id="yDmH0d"]'), 1000))).catch(function(err) {
+    console.log("activitycontrols error");
+  });
+
+  await editCheckbox(false, 1); //webActivity
+  await editCheckbox(true, 2); //location history
+  await editCheckbox(false, 7); // device information
+  await editCheckbox(true, 5); // voice activity
+  await editCheckbox(true, 3); // youtube search history
+  await editCheckbox(false, 4); // youtube play history
+  await editCustomAds(true);
+  await driver.quit();
+}
+
+async function editCheckbox(toggle, sid) {
   await new Promise(resolve => setTimeout(resolve, 500));
-  await driver.findElement(By.xpath("//button[@id='settings_save']")).click();
+  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//div[@jsname="ilpChd" and @data-sid=\"' + sid + '\"]'), 1000))).catch(function(err) {
+    console.log("find checkbox error");
+  });
+  const elem = await driver.findElement(By.xpath('//div[@jsname="ilpChd" and @data-sid=\"' + sid + '\"]'));
+  var isChecked = toBoolean("false");
+  await elem.getAttribute('aria-checked').then(function(val) {
+    isChecked = toBoolean(val);
+    // console.log("aria-checked sid : " + sid + " val(Boolean):" + toBoolean(val) + " val:" + val);
+  }).catch(function(err) {
+    console.log("aria-checked error " + err);
+  })
 
-  const auth_pass = await driver.findElement(By.xpath("//input[@id='auth_password']"));
-  await auth_pass.sendKeys(password);
-  // await new Promise(resolve => setTimeout(resolve, 500));
-  await auth_pass.sendKeys(Key.ENTER);
+  if ((isChecked == false && toggle == true) || (isChecked == true && toggle == false)) {
+    await elem.click();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//div[@data-id="EBS5u"]/content/span'), 1000))).catch(function(err) {
+      console.log("cannot find checkbox confirm " + err);
+    })
+    const e = await driver.findElement(By.xpath('//div[@data-id="EBS5u"]/content/span')).catch(function(err) {
+      console.log("cannot find checkbox confirm 2 " + err);
+    })
+    await clickConfirmButton(e);
+
+    async.whilst(
+      function() {
+        return driver.findElement(By.xpath('//div[@data-id="EBS5u"]/content/span')).then(function(data) {
+          return true;
+        }).catch(function(err) {
+          return false;
+        })
+      },
+      function(callback) {
+
+      },
+      function(err, n) {
+        console.log("error whilst : " + err);
+      }
+    );
+    await new Promise(resolve => setTimeout(resolve, 500));
+  }
+}
+
+async function editCustomAds(toggle){
+  await new Promise(resolve => setTimeout(resolve, 500));
+  await driver.get('https://adssettings.google.com/authenticated');
+  await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//div[@jsname="ornU0b"]'), 1000))).catch(function(err) {
+    console.log("activitycontrols error");
+  });
+  const elem = await driver.findElement(By.xpath('//div[@jsname="ornU0b"]'), 1000);
+  var isChecked = toBoolean("false");
+  await elem.getAttribute('aria-checked').then(function(val){
+    isChecked = toBoolean(val);
+  }).catch(function(err){
+
+  })
+
+  if ((isChecked == false && toggle == true) || (isChecked == true && toggle == false)) {
+    await elem.click();
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    await driver.wait(until.elementIsVisible(driver.findElement(By.xpath('//div[@jsname="j6LnYe"]/content/span'), 1000))).catch(function(err) {
+      console.log("cannot find checkbox confirm " + err);
+    })
+    const e = await driver.findElement(By.xpath('//div[@jsname="j6LnYe"]/content/span')).catch(function(err) {
+      console.log("cannot find checkbox confirm 2 " + err);
+    })
+    await clickConfirmButton(e);
+
+    async.whilst(
+      function() {
+        return driver.findElement(By.xpath('//div[@jsname="j6LnYe"]/content/span')).then(function(data) {
+          return true;
+        }).catch(function(err) {
+          return false;
+        })
+      },
+      function(callback) {
+
+      },
+      function(err, n) {
+        console.log("error whilst : " + err);
+      }
+    );
+    await new Promise(resolve => setTimeout(resolve, 1000));
+  }
 }
 
 
-exports.login = function(username,password) {
+async function clickConfirmButton(e) {
+  try {
+    const a = await (() => {
+      return new Promise((resolve, reject) => {
+        setInterval(function() {
+          e.click().then(function(data) {
+            // console.log("click!! confirm button");
+          }).catch(function(err) {
+            clearInterval(this);
+            resolve("");
+          })
+        }, 2000);
+      })
+    })();
+    // return console.log(a);
+  } catch (err) {
+    return console.error("clickConfirmButton error : " + err);
+  }
+}
+
+function toBoolean(str) {
+  return (str == 'true') ? true : false;
+}
+
+
+
+var username = "kihalllllo";
+var password = "ekuadoru";
+
+exports.login = function(user, pass) {
+  username = user;
+  password = pass;
   main(username,password)
     .then((result) => {
       return true;
     });
 }
+
+exports.privacy_setting = function(index) {
+  setting(index)
+    .then((result) => {
+      return true;
+    })
+}
+
+// main(username, password)
+//   .then((result) => {
+//
+//   });
